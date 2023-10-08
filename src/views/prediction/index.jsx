8766b12f4a97d0e65ab65ecedf0ba26633c0bdcd
Andres Cano <andrcnca@suramericana.com.co>
2023-10-08 17:39:48 -0500

    Se integra el formulario con el back

--- src/views/prediction/index.jsx
+++ src/views/prediction/index.jsx
@@ -7,24 +7,33 @@ import { useState } from "react";
 const Prediction = () => {
     const [response, setResponse] = useState();
 
-    const getPrediction = () =>{
-        const url = 'https://5abf-35-221-17-90.ngrok-free.app/predict';
+    const getPrediction = (phi, dens, speed, temp) => {
+        const url = 'http://ae8d-35-221-17-90.ngrok-free.app/predict';
         const data = new FormData();
-        data.append('Phi', 51.66);
-        data.append('Dens',6.02);
-        data.append('Speed',455.7);
-        data.append('Temp',188930);
-    
+        data.append('Phi', parseFloat(phi));
+        data.append('Dens', parseFloat(dens));
+        data.append('Speed', parseFloat(speed));
+        data.append('Temp', parseFloat(temp));
+
         fetch(url, {
             method: 'POST',
             body: data
         }).then(res => res.json())
-        .then(res => setResponse(res[0] === 1?"Solar Storm was detected": "No Solar Storm was detected"));
+            .then(res => setResponse(res[0] === 1 ? "Solar Storm was detected" : "No Solar Storm was detected"));
     }
 
     /*const getPrediction = (values) =>{
        setResponse("No Solar Storm was detected");
     }*/
+
+
+    const handleSubmit = (values) => {
+        values.preventDefault()
+        getPrediction(values.target.phi.value,
+            values.target.density.value,
+            values.target.speed.value,
+            values.target.temperature.value);
+    }
 
 
     return (
@@ -38,53 +47,37 @@ const Prediction = () => {
 
 
             </ReactNebula>
-            <Formik
-            initialValues={{Phi: '', Dens: '', Speed: '',Temp:'', test: ''}}
-            onSubmit={(values) => {
-                console.log(values)
-            }}>
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                    /* and other goodies */
-                }) => {
-                    return (
-                        <div class="container">
-                            <FormGroup style={{ position: "absolute" }} onSubmit={handleSubmit}>
-                                <FormControl focused>
-                                    <InputLabel color="warning" focused htmlFor="date">PHI</InputLabel>
-                                    <Input style={{ color: "white" }} type="number" color="warning" name="phi" id="phi" />
-                                </FormControl>
-                    
-                                <FormControl >
-                                    <InputLabel color="warning" focused htmlFor="temperature">Temperature</InputLabel>
-                                    <Input style={{ color: "white" }} type="number" color="warning" name="temperature" id="temperature" />
-                                </FormControl>
-                                <FormControl>
-                                    <InputLabel color="warning" htmlFor="density" focused>Density</InputLabel>
-                                    <Input style={{ color: "white" }} type="number" name="density" color="warning" id="density" />
-                                </FormControl>
-                                <FormControl className="input-number">
-                                    <InputLabel color="warning" htmlFor="speed" focused>Speed</InputLabel>
-                                    <Input type="number" style={{ color: "white" }} color="warning" name="speed" id="speed" />
-                                </FormControl>
-    
-                                <Button type="submit" variant="contained" color="success" onClick={(values) => getPrediction()}>
-                                    Predict
-                                </Button>
-                                <p style={{color:"white"}}>{response}</p>
-                            </FormGroup>
-                        </div>
-                    );
 
-                }
-                }
-            </Formik>
+                <form onSubmit={handleSubmit}>
+            <div class="container">
+                    <FormGroup style={{ position: "absolute" }} onSubmit={handleSubmit}>
+                        <FormControl focused>
+                            <InputLabel color="warning" focused htmlFor="date">PHI</InputLabel>
+                            <Input step="0.01" style={{ color: "white" }} type="text" color="warning" name="phi" id="phi" />
+                        </FormControl>
+
+                        <FormControl >
+                            <InputLabel color="warning" focused htmlFor="temperature">Temperature</InputLabel>
+                            <Input step="0.01" style={{ color: "white" }} type="text" color="warning" name="temperature" id="temperature" />
+                        </FormControl>
+                        <FormControl>
+                            <InputLabel color="warning" htmlFor="density" focused>Density</InputLabel>
+                            <Input step="0.01" style={{ color: "white" }} type="text" name="density" color="warning" id="density" />
+                        </FormControl>
+                        <FormControl className="input-number">
+                            <InputLabel color="warning" htmlFor="speed" focused>Speed</InputLabel>
+                            <Input step="0.01" type="text" style={{ color: "white" }} color="warning" name="speed" id="speed" />
+                        </FormControl>
+                        <Button type="submit" variant="contained" color="success">
+                            Predict
+                        </Button>
+                        <p style={{ color: "white" }}>{response}</p>
+                    </FormGroup>
+            </div>
+                </form>
+
+
+
         </div>
     )
 }
